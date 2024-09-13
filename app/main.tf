aebf3b85059391735e30e5295289ed0759559aa2
nzapanarcisse <nzapanarcisse@gmail.com>
2024-09-13 15:47:27 +0400

    narcisse

---
+++ app/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region                   = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-backend-lepro"
+    key                     = "Lepro-dev.tfstate"
+    region                  = "us-east-1"
+  }
+}
+
+
+# Création du sg
+module "sg" {
+  source = "../modules/sg"
+}
+
+# Création de l'EIP
+module "eip" {
+  source = "../modules/eip"
+}
+
+# Création de l'EC2 
+module "ec2" {
+  source        = "../modules/ec2"
+  instance_type = "t2.micro"
+  public_ip     = module.eip.output_eip
+  sg_name       = module.sg.output_sg_name
+  server_name   = "ic_webapp_server_dev"
+
+}
+
+#Creation des associations nécessaires
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = module.ec2.output_ec2_id
+  allocation_id = module.eip.output_eip_id
+}
+
+resource "null_resource" "provision" {
+  provisioner "local-exec" {
+    command = "ansible-playbook ../ansible/insert_host.yml"
+  }
+
+  # Ajoutez une dépendance pour s'assurer que l'instance est créée avant d'exécuter les playbooks
+    depends_on = [module.ec2]
+}
---
+++ modules/ec2/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "ic-webapp-ec2" {
+  ami               = "ami-0e86e20dae9224db8"
+  instance_type     = var.instance_type
+  key_name          = var.ssh_key
+  availability_zone = var.AZ
+  security_groups   = ["${var.sg_name}"]
+  tags = {
+    Name = "${var.maintainer}-ec2"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${var.public_ip} > /home/nzapa/projet/easytraining/datascientestTraining/datascientest_terraform/public_ip.txt"
+  }
+
+}
+
