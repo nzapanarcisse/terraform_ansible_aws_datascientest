26f2381e588b8a99817c863b0df517dfccc41b29
nzapanarcisse <nzapanarcisse@gmail.com>
2024-11-07 13:21:34 +0400

    commiet

--- app/main.tf
+++ app/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket                  = "terraform-backend-lepro"
+    bucket                  = "terraform-backend-lepros"
     key                     = "Lepro-dev.tfstate"
     region                  = "us-east-1"
   }
